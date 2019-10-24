da4594235227da0169b79acf87795e82d31603e0
Alexis Moragues <alexis.moragues@wolox.com.ar>
2019-10-24 14:35:49 -0300

    delete defaults and apollo server

--- app/graphql/gitChecks/type_defs.js
+++
@@ -1,9 +0,0 @@
-const { gql } = require('apollo-server');
-
-const rootTypes = gql`
-  extend type Query {}
-  extend type Mutation {}
-  extend type Subscription {}
-`;
-
-exports.typeDefs = [rootTypes];
