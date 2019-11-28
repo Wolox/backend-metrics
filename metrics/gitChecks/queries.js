284b138d18c3228ab4a95bda41ba98db49056c91
Alexis Moragues <alexis.moragues@wolox.com.ar>
2019-11-28 13:00:00 -0300

    eslint disable for useless invocations

--- metrics/gitChecks/queries.js
+++ metrics/gitChecks/queries.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const { request } = require('graphql-request');
 const { GraphQLString } = require('graphql');
 const { gitStatsByTech } = require('./types');
@@ -9,13 +10,14 @@ const generateGitStatsArgsString = args => {
     const auxString = `${key}: "${args[key]}"`;
     if (stringArgs === '') {
       stringArgs = stringArgs.concat(auxString);
+    } else {
+      stringArgs = stringArgs.concat(', ', auxString);
     }
-    else stringArgs = stringArgs.concat(', ', auxString);
   });
   return stringArgs;
 };
 
-const getAvgPickUpTime = async (args) => {
+const getAvgPickUpTime = async args => {
   const argsString = generateGitStatsArgsString(args || {});
   const query = `query{stats {\
     pr_pick_up_time_avg(${argsString}) {\
