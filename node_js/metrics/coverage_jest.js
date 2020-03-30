ff53e7135b3efda385d2fccdc15bd73be7439944
Gonzalo Escandarani <gonziesc@gmail.com>
2020-03-30 14:38:44 -0300

    adding better coverage way

--- node_js/metrics/coverage_jest.js
+++ node_js/metrics/coverage_jest.js
@@ -6,30 +6,24 @@ const FUNCTIONOFFSET = 95;
 const LINESOFFSET = 106;
 const PERCENTAGEPRECISION = 5;
 
-exports.checkCoverage = (testPath) => {
+exports.checkCoverage = async (testPath) => {
   console.log('Empezando coverage para el build...');
   const metrics = [];
-  const results = shell.exec(
+  await shell.exec(
     `npm run cover ${testPath}`
   );
-  const allFilesIndex = results.stdout.search('\nAll files');
-  const functionPercent = results.stdout.slice(
-    allFilesIndex + FUNCTIONOFFSET,
-    allFilesIndex + FUNCTIONOFFSET + PERCENTAGEPRECISION
-  );
+  const metricsFile = require(`${testPath}/coverage/coverage-summary.json`);
+  const functionPercent = metricsFile.total.functions.pct;
   metrics.push({
     metric: 'Functions Covered',
     description: '% de funciones cubiertas',
-    value: parseInt(functionPercent[2] === "." ? functionPercent : functionPercent.slice(0,1))
+    value: parseFloat(functionPercent)
   });
-  const linesPercent = results.stdout.slice(
-    allFilesIndex + LINESOFFSET,
-    allFilesIndex + LINESOFFSET + PERCENTAGEPRECISION
-  );
+  const linesPercent = metricsFile.total.lines.pct;
   metrics.push({
     metric: 'Lines covered',
     description: '% de lineas cubiertas',
-    value: parseInt(linesPercent[2] === "." ? linesPercent : linesPercent.slice(0,2))
+    value: parseFloat(linesPercent)
   });
   return metrics;
 };
