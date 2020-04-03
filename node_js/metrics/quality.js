00d981fdc1b7700d226c38383f67569d951f4098
Gonzalo Escandarani <gonziesc@gmail.com>
2020-04-03 13:00:19 -0300

    trying global jsinspect

--- node_js/metrics/quality.js
+++ node_js/metrics/quality.js
@@ -6,8 +6,8 @@ const results = [];
 exports.checkInspect = testPath => new Promise(resolve => {
   let score = 100;
   let instances = 5;
-  const proc = spawn.sync('/var/lib/jenkins/workspace/backend-metrics/node_js/node_modules/.bin/jsinspect', ['-I', '-L', '-m' ,instances, '-t' ,'20', '--ignore' ,"migrations|test|coverage", '--reporter', 'json'],
-  { cwd: testPath, stdio: 'inherit' });
+  const proc = spawn.sync('jsinspect', ['-I', '-L', '-m' ,instances, '-t' ,'20', '--ignore' ,"migrations|test|coverage", '--reporter', 'json'],
+  { cwd: testPath });
   console.log(proc);
   const matches = JSON.parse(proc.stdout).map(i => parseFloat(i.instances.length / 10)).reduce((prev, next) => prev + next, 0);
   score = score - matches * (instances / 4);
