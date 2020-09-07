101ef5fd954ee03282ce6690950d30d2df67dedc
Samir Tapiero <50454914+blacksam07@users.noreply.github.com>
2020-09-07 14:56:01 -0500

    Add metrics.sh and fix problem with rspec report and build time (#26)
    * Add metrics.sh and fix problem with rspec report and build time
    
    * Add changes in README and add new empty line

--- ruby_on_rails/metrics.sh
+++ ruby_on_rails/metrics.sh
@@ -1,43 +1,3 @@
-# Backend Metrics
-
-## Configuration
-
-### Ruby on Rails
-
-```json
-# .woloxci/metrics.json
-
-{
-  "baseBranch": "development",
-  "projectName": "'Change me with project name'",
-  "tech": "ruby_on_rails",
-  "repoName": "repository-name",
-  "apmProjectName": "repository-apm-if-exists"
-}
-```
-
-In `metrics` step, we should use the same configuration for  `repoName`, `projectName` and `baseBranch` used in `metrics.json` file
-
-```yml
-# .woloxci/config.yml
-
-steps:
-  config:
-    - rm -rf metrics && mkdir metrics && wget -O metrics/metrics.sh https://raw.githubusercontent.com/Wolox/backend-metrics/3493da65a51e7800d0a857cc4fd197f278e6e74f/ruby_on_rails/metrics.sh && chmod +x metrics/metrics.sh
-    - bundle install | tee -a metrics/bundle_install
-  analysis:
-    - bundle exec rubycritic --path ./analysis --minimum-score 80 --no-browser | tee -a metrics/rubycritic_report
-  test:
-    - bundle exec rspec | tee -a metrics/rspec_report
-  metrics:
-    - ./metrics/metrics.sh repository-name 'Change me with project name' development
-```
-
-
-
-```bash
-# .woloxci/metrics.sh
-
 # Move to project's root folder
 cd metrics
 
@@ -130,22 +90,4 @@ curl -i \
       }
     ]
   }'
-
-```
-
-## Contributing
-
-1. Fork it
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -am 'Add some feature'`)
-4. Run rspec tests (`bundle exec rspec spec -fd`)
-5. Run scss lint (`bundle exec scss-lint app/assets/stylesheets/`)
-6. Run rubocop lint (`bundle exec rubocop app spec -R`)
-7. Push your branch (`git push origin my-new-feature`)
-8. Create a new Pull Request
-
-## About
-
-This project is written by [Wolox](http://www.wolox.com.ar).
-
-[![Wolox](https://raw.githubusercontent.com/Wolox/press-kit/master/logos/logo_banner.png)](http://www.wolox.com.ar)
+  
